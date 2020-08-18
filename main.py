5f67825a15d480b51dbd90d122c17eb61889cdc4
alan <alan@voxel51.com>
2020-08-18 12:13:30 -0400

    Add support for forks

--- main.py
+++ main.py
@@ -32,23 +32,27 @@ slack_url = os.environ["SLACK_WEBHOOK_URL"]
 if not slack_url.startswith("https://"):
     raise ValueError("Invalid SLACK_WEBHOOK_URL:", slack_url)
 
+def scan(*, noun, redis_key, api_route, name_key):
+    first_run = not redis_client.exists(redis_key)
+    res = requests.get(f"https://api.github.com/repos/{repo}/{api_route}").json()
+    print(f"{api_route}: fetched {len(res)} results")
+    new_users = [u for u in res
+        if redis_client.sadd(redis_key, u[name_key])]
+    for u in new_users:
+        if first_run:
+            print(f"existing {noun}: {u[name_key]}")
+        else:
+            print(f"new {noun}: {u[name_key]}")
+    if new_users and not first_run:
+        user_text = ", ".join(f"<{u['html_url']}|{u[name_key]}>" for u in new_users)
+        desc_text = f"{len(new_users)} new {noun}s" if len(new_users) > 1 else f"New {noun}"
+        message = {"text": f"{desc_text}: {user_text}"}
+        requests.post(slack_url, json=message)
+
 while True:
     try:
-        first_run = not redis_client.exists("stars")
-        res = requests.get(f"https://api.github.com/repos/{repo}/stargazers").json()
-        print(f"fetched {len(res)} results")
-        new_users = [u for u in res
-            if redis_client.sadd("stars", u["login"])]
-        for u in new_users:
-            if first_run:
-                print("existing star", u["login"])
-            else:
-                print("new star", u["login"])
-        if new_users and not first_run:
-            user_text = ", ".join("<{html_url}|{login}>".format(**u) for u in new_users)
-            star_text = f"{len(new_users)} new stars" if len(new_users) > 1 else "New star"
-            message = {"text": f"{star_text}: {user_text}"}
-            requests.post(slack_url, json=message)
+        scan(noun="star", redis_key="stars", api_route="stargazers", name_key="login")
+        scan(noun="fork", redis_key="forks", api_route="forks", name_key="full_name")
 
     except Exception:
         traceback.print_exc()
